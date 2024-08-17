bd0e23432cccf4fec727d26f1e36f3335d1e6420
backdoorlp <rapha.b.o@hotmail.com>
2024-08-17 11:20:41 -0300

    Add files via upload
    Atividade feita no Senac Catanduva 17/08/2024

---
+++ Conversor de Moedas.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploDo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double qtdreal, qtddolar, valordolar, convertidoreal, convertidodolar;
+            int op;
+
+            Console.WriteLine("Conversor de moedas");
+            Console.WriteLine("Digite {1} para converter Real em Dolar, Digite {2} Para converter Dolar em Real ");
+            op = Convert.ToInt32(Console.ReadLine());
+
+            if (op == 1)
+            {
+                Console.WriteLine("Quanto esta valendo o dolar");
+                valordolar = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Qual Quantia em Real voce deseja Converter?");
+                qtdreal = double.Parse(Console.ReadLine());
+
+                convertidoreal = qtdreal / valordolar;
+
+                Console.WriteLine("A Conversão de R$" + qtdreal + " é: " + "$" + convertidoreal);
+            }
+            else if (op == 2)
+            {
+                Console.WriteLine("Quanto esta Valendo o dolar: ");
+                valordolar = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Qual Quantia em dolares voce deseja converter: ");
+                qtddolar = double.Parse(Console.ReadLine());
+
+                convertidodolar = qtddolar * valordolar;
+
+                Console.WriteLine("A Conversão de $" + qtddolar + " é: " + "R$" + convertidodolar);
+            }
+            else
+            {
+                Console.WriteLine("Você Não Selecionou Uma Opção Válida!!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
